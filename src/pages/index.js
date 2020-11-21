6a6c41a4753754e205d6631a4d482094a18e7313
Danny Li <dongledan@yahoo.com>
2020-11-21 00:19:02 -0500

    downgraded sass bc sass-loader error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import '../scss/index.scss'
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
